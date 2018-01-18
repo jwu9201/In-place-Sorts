616eb6bac9a090255661d1167e451b6ed1feb174
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2018-01-18 09:30:15 -0500

    asdf

--- ArrayMethods1.java
+++ ArrayMethods1.java
@@ -1,10 +1,15 @@
 
 public class ArrayMethods1 {
 	
-	public static void insertionSort(int[] list1) {
-		for (int i = 0; i < list1.length-2; i++) {
-			Swap(list1, list1[i], list1[i+1]);
+	public static void notInsertionSort(int[] list1) {
+		for (int j = 0; j < list1.length;j++) {
+			for (int i = 0; i < list1.length-1; i++) {
+			Swap(list1, i, i+1);
+			}
 		}
+	}
+	public static void insertionSort(int[] list1) {
+		
 	}
 	
 	public static void selectionSort(double[] list1) {
@@ -12,13 +17,34 @@ public class ArrayMethods1 {
 	}
 	
 	public static void bubbleSort(String[] list1) {
+
+			for (int i = 0; i < list1.length-1; i++) {
+				Swap(list1, i, i+1);
+			}
 		
 	}
 	
 	public static void Swap(int[] arr, int index1, int index2) {
-		int x = arr[index1];
-		arr[index1] = arr[index2];
-		arr[index2] = arr[x];
+		if (arr[index1] > arr[index2]) {
+			int x = arr[index1];
+			arr[index1] = arr[index2];
+			arr[index2] = x;
+		}
 	}
-
+	
+	public static void Swap(double[] arr,int index1, int index2) {
+		if (arr[index1] > arr[index2]) {
+			double x = arr[index1];
+			arr[index1] = arr[index2];
+			arr[index2] = x;
+		}
+	}
+	
+	public static void Swap(String[] arr,int index1, int index2) {
+		if ((arr[index1].compareTo(arr[index2]) > 0)) {
+			String x = arr[index1];
+			arr[index1] = arr[index2];
+			arr[index2] = x;
+		}
+	}
 }
--- Main.java
+++ Main.java
@@ -4,12 +4,20 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int[] list1 = {1, 2, 3, 4, 5};
-		double[] list2 = {5,4,3,2,1};
-		String[] list3 = {"5, 4, 3, 2, 1"};
+		int[] list1 = {5, 3, 4, 1, 7};
+		double[] list2 = {5,3,7,9,1};
+		String[] list3 = {"d", "f", "t", "q", "a"};
 		
-		ArrayMethods1.insertionSort(list1);
-	}
+		//ArrayMethods1.notInsertionSort(list1);
+		//for (int x: list1) {
+		//	System.out.println(x);
+		//}
+		
+		ArrayMethods1.bubbleSort(list3);
+		for (String x: list3) {
+			System.out.println(x);
+		}
 
+	}
 }
 
