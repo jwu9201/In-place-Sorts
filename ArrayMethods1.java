ac1f85b5186a5703f823c04c11b8c7c2dc0506e0
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2018-01-19 09:30:30 -0500

    asdf

--- ArrayMethods1.java
+++ ArrayMethods1.java
@@ -1,18 +1,23 @@
 
 public class ArrayMethods1 {
 	
-	public static void notInsertionSort(int[] list1) {
-		for (int j = 0; j < list1.length;j++) {
-			for (int i = 0; i < list1.length-1; i++) {
-			Swap(list1, i, i+1);
-			}
-		}
-	}
 	public static void insertionSort(int[] list1) {
 		
 	}
 	
 	public static void selectionSort(double[] list1) {
+		for (int j = 0; j < list1.length;j++) {
+			int pos = j;
+			for (int i = 0+j; i < list1.length; i++) {
+				double x = list1[j];
+				if (x > list1[i]) {
+					x = list1[i];
+					pos = i;
+				}
+			}
+			System.out.println(pos);
+			Swap(list1, pos , j);
+		}
 		
 	}
 	
@@ -33,12 +38,12 @@ public class ArrayMethods1 {
 	}
 	
 	public static void Swap(double[] arr,int index1, int index2) {
-		if (arr[index1] > arr[index2]) {
-			double x = arr[index1];
-			arr[index1] = arr[index2];
-			arr[index2] = x;
-		}
+		double x = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = x;
 	}
+	
+	
 	
 	public static void Swap(String[] arr,int index1, int index2) {
 		if ((arr[index1].compareTo(arr[index2]) > 0)) {
--- Main.java
+++ Main.java
@@ -5,13 +5,18 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 		int[] list1 = {5, 3, 4, 1, 7};
-		double[] list2 = {5,3,7,9,1};
+		double[] list2 = {5, 3, 7, 9, 1};
 		String[] list3 = {"d", "f", "t", "q", "a"};
 		
 		//ArrayMethods1.notInsertionSort(list1);
 		//for (int x: list1) {
 		//	System.out.println(x);
 		//}
+		
+		ArrayMethods1.selectionSort(list2);
+		for (double x: list2) {
+			System.out.println(x);
+		}
 		
 		ArrayMethods1.bubbleSort(list3);
 		for (String x: list3) {
@@ -20,4 +25,5 @@ public class Main {
 
 	}
 }
+
 
